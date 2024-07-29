1aae0835f8b0c047f85d86fb4e6ac0c8469c8e05
Florencia Zoffi <95873342+florenciazoffi@users.noreply.github.com>
2024-07-29 08:16:52 -0500

    starting and ending interfaces
    added two functions for starting and ending interfaces to show

--- puzzle.py
+++ puzzle.py
@@ -54,3 +54,47 @@ def getImagePaths( rootdir ):
     assert len( image_names ) > 0
     return os.path.join( rootdir, random.choice( image_names ) )
 
+def showEndInterface( screen, width, height ):
+    screen.fill( configuration.BACKGROUND_COLOR )
+    font = pygame.font.Font( configuration.FONTPATH, width/15 )
+    title = font.render( 'Good job, you won!', True, ( 233, 150, 122 ) )
+    rect = title.get_rect()
+    rect.midtop = ( width/2, height/2.5 )
+    screen.blit( title, rect )
+    pygame.display.update()
+    while True:
+        for event in pygame.event.get():
+            if( event.type == pygame.QUIT ) or ( event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE ):
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+
+def showStartInterface( screen, width, height ):
+    screen.fill( configuration.BACKGROUND_COLOR )
+    t_font = pygame.font.Font( configuration.FONTPATH, width/4 ) # el puso // en vez de /
+    c_font = pygame.font.Font( configuration.FONTPATH, width/20 ) #
+
+    title = t_font.render( 'Puzzle', True, configuration.RED )
+    content1 = c_font.render( 'Press H, M or L to choose your puzzle', True, configuration.BLUE )
+    content2 = c_font.render( 'H-5x5, M-4x4, L-3x3', True, configuration.BLUE )
+    t_rect = title.get_rect()
+    t_rect.midtop = ( width/2, height/10 )
+    c_rect1 = content1.get()
+    c_rect1.midtop = ( width/2, height/2.2 )
+    c_rect2 = content2.get()
+    c_rect2.midtop = ( width/2, height/1.8 )
+
+    screen.blit( title, t_rect )
+    screen.blit( content1, c_rect1 )
+    screen.blit( content2, c_rect2 )
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or ( event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE ):
+                pygame.quit()
+                sys.exit
+            elif event.type == pygame.KEYDOWN:
+                if event.key == ord( 'l' ): return 3
+                elif event.key == ord( 'm' ): return 4
+                elif event.key == ord( 'h' ): return 5
+        pygame.display.update()
