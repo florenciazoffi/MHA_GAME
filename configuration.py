dc88f289efc8be85888599fcd2653fbc866a45dc
Florencia Zoffi <95873342+florenciazoffi@users.noreply.github.com>
2024-07-28 19:20:13 -0500

    configuration file

---
+++ configuration.py
@@ -0,0 +1,13 @@
+import os
+
+SCREENSIZE = ( 640, 640 )
+PICTURE_ROOT_DIR = os.path.join( os.getcwd(), 'resources/pictures' )
+FONTPATH = os.path.join( os.getcwd(), 'resources/font/AvenirCopy.ttf')
+BACKGROUND_COLOR = ( 255, 255, 255 )
+
+RED = ( 255, 0 , 0 )
+BLUE = ( 0, 0, 255 )
+BLACK = ( 0, 0, 0 )
+
+FPS = 40  
+RANDNUM = 100
